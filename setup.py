31f6b9d92ca1467f55c173de58630cf0906f5512
Nikita Lvov <lvov@tugraz.at>
2020-10-12 11:08:42 +0200

    configuration fixes #2

--- setup.py
+++ setup.py
@@ -13,14 +13,6 @@ readme = open('README.rst').read()
 history = open('CHANGES.rst').read()
 
 tests_require = [
-    'check-manifest>=0.25',
-    'coverage>=4.0',
-    'isort>=4.3.3',
-    'pydocstyle>=1.0.0',
-    'pytest-cache>=1.0',
-    'pytest-cov>=1.8.0',
-    'pytest-pep8>=1.0.6',
-    'pytest>=2.8.0',
     'pytest-invenio>=1.0.1',
     'invenio-app>=1.0.1',
     'redis>=2.10.6'
