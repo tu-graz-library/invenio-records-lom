081a0100297e195fb2e99961dd53f15918dd7826
Martin Obersteiner <obersteiner@tugraz.at>
2021-11-25 12:58:14 +0100

    remove now unnecessary fix

--- docs/conf.py
+++ docs/conf.py
@@ -109,7 +109,6 @@ pygments_style = "sphinx"
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
-nitpick_ignore = [("py:class", "flask.app.Flask")]
 
 # -- Options for HTML output ----------------------------------------------
 html_theme = "alabaster"
