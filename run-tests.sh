ed22d3359313edf6165d18cc81c605b1d3df3fb7
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-10-11 16:38:58 +0200

    fix: change opensearch2 to opensearch in run-tests
    * opensearch2 does not exists as a docker image
    
    * move the requirements ignore thing to setup.cfg

--- run-tests.sh
+++ run-tests.sh
@@ -24,7 +24,7 @@ function cleanup() {
 trap cleanup EXIT
 
 
-python -m check_manifest --ignore ".*-requirements.txt"
+python -m check_manifest
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
-eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch2} --cache ${CACHE:-redis} --env)"
-python -m pytest -s
+eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch} --cache ${CACHE:-redis} --env)"
+python -m pytest
