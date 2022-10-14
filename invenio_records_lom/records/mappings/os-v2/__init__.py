76fa2938bbb24487e9ca2d2a45a300b08df32e15
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-10-14 08:36:35 +0200

    typo: fixed wrong position of .

--- invenio_records_lom/records/mappings/os-v2/__init__.py
+++ invenio_records_lom/records/mappings/os-v2/__init__.py
@@ -5,4 +5,4 @@
 # invenio-records-lom is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""Opensearch .2* mappings for LOM records and drafts."""
+"""Opensearch 2.* mappings for LOM records and drafts."""
