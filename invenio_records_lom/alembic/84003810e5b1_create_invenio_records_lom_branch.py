88d05a86db4f90a8d75b4095c93f7b145e632bc2
Philipp Gualdi <philipp.gualdi@tugraz.at>
2023-05-10 13:41:00 +0000

    modification: alembic scripts
    * initialize alembic upgrade scripts

---
+++ invenio_records_lom/alembic/84003810e5b1_create_invenio_records_lom_branch.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023 Graz University of Technology.
+#
+# invenio-records-lom is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Create invenio records lom branch."""
+
+# revision identifiers, used by Alembic.
+revision = "84003810e5b1"
+down_revision = None
+branch_labels = ("invenio_records_lom",)
+depends_on = "dbdbc1b19cf2"
+
+
+def upgrade():
+    """Upgrade database."""
+
+
+def downgrade():
+    """Downgrade database."""
