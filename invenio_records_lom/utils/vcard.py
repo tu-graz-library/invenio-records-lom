531737da892d22803af59298e61b7fb09492259d
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-01-28 11:16:09 +0100

    black: fix formating v24.1.1

--- invenio_records_lom/utils/vcard.py
+++ invenio_records_lom/utils/vcard.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2023 Graz University of Technology.
+# Copyright (C) 2023-2024 Graz University of Technology.
 #
 # invenio-records-lom is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -37,7 +37,8 @@ class VCardProperty:
 
     compoundable: bool
     min: int = 0
-    max: int = 255  # just some sufficiently high number for properties that can appear arbitrarily often
+    # just some sufficiently high number for properties that can appear arbitrarily often
+    max: int = 255
 
 
 DEFAULT_PROPERTIES_CONFIG: dict[str, VCardProperty] = {
