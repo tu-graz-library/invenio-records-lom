c270a1073b3d981b76005a856e1845d702cbdcf0
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-02-01 21:29:07 +0100

    tests: update run-tests to invenio standard

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,7 @@
 #
 # Copyright (C) 2019-2020 CERN.
 # Copyright (C) 2019-2020 Northwestern University.
-# Copyright (C) 2020-2023 Graz University of Technology.
+# Copyright (C) 2020-2024 Graz University of Technology.
 #
 # invenio-records-lom is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -20,10 +20,32 @@ set -o nounset
 function cleanup() {
     eval "$(docker-services-cli down --env)"
 }
-trap cleanup EXIT
+
+# Check for arguments
+# Note: "-k" would clash with "pytest"
+keep_services=0
+pytest_args=()
+for arg in $@; do
+    # from the CLI args, filter out some known values and forward the rest to "pytest"
+    # note: we don't use "getopts" here b/c of some limitations (e.g. long options),
+    #       which means that we can't combine short options (e.g. "./run-tests -Kk pattern")
+    case ${arg} in
+	-K|--keep-services)
+	    keep_services=1
+	    ;;
+	*)
+	    pytest_args+=( ${arg} )
+	    ;;
+    esac
+done
+
+if [[ ${keep_services} -eq 0 ]]; then
+    trap cleanup EXIT
+fi
+
 
 
 python -m check_manifest
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch} --env)"
-python -m pytest
+python -m pytest ${pytest_args[@]+"${pytest_args[@]}"}
