841852092868f5b22fba86e97b7ce0040c8c203e
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-06-17 13:28:15 +0200

    fix: record vs record_ui
    * there where changes in app-rdm about the usage of record vs record_ui,
      one is the serialized object the other not

--- invenio_records_lom/ui/records/records.py
+++ invenio_records_lom/ui/records/records.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2019-2021 CERN.
 # Copyright (C) 2019-2021 Northwestern University.
 # Copyright (C)      2021 TU Wien.
-# Copyright (C) 2021-2024 Graz University of Technology.
+# Copyright (C) 2021-2025 Graz University of Technology.
 #
 # invenio-records-lom is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -128,7 +128,8 @@ def record_detail(
             ["edit", "new_version", "manage", "update_draft", "read_files", "review"],
         ),
         pid=pid_value,
-        record=record_ui,
+        record=record,
+        record_ui=record_ui,
     )
 
 
