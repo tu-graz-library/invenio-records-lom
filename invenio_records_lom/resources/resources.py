fcaed7df63de2bda726eaa27a3f8720629d8dd94
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-02-05 08:31:43 +0100

    fix: links deprecation warning

--- invenio_records_lom/resources/resources.py
+++ invenio_records_lom/resources/resources.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2021-2024 Graz University of Technology.
+# Copyright (C) 2021-2026 Graz University of Technology.
 #
 # invenio-records-lom is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -27,14 +27,13 @@ class LOMRecordResource(RDMRecordResource):
 
         routes = self.config.routes
         return [
-            route("DELETE", prefix(routes["item-pids-reserve"]), self.pids_discard),
             route("DELETE", prefix(routes["item-draft"]), self.delete_draft),
             route("GET", prefix(routes["list"]), self.search),
             route("GET", prefix(routes["item"]), self.read),
             route("POST", prefix(routes["list"]), self.create),
             route("POST", prefix(routes["item-draft"]), self.edit),
+            route("GET", prefix(routes["item-draft"]), self.read_draft),
             route("POST", prefix(routes["item-publish"]), self.publish),
-            route("POST", prefix(routes["item-pids-reserve"]), self.pids_reserve),
             route("PUT", prefix(routes["item-draft"]), self.update_draft),
             # User Dashboard routes
             route("GET", s(routes["user-prefix"]), self.search_user_records),
