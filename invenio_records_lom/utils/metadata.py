5fc98e749d50d41000f5b68f5ba925d380ef27e0
Thomas Schöngaßner <thomas.schoengassner@boku.ac.at>
2026-04-10 15:43:35 +0200

    docs(metadata): adding boku to copyright in context of commit f0663c61465b11f0c755b7f24a4361759d6effd2

--- invenio_records_lom/utils/metadata.py
+++ invenio_records_lom/utils/metadata.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2022-2025 Graz University of Technology.
+# Copyright (C) 2025 BOKU University.
 #
 # invenio-records-lom is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
