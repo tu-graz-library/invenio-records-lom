77a549fc5e3e12b998d2d7bf93629063c15ad354
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-04-28 11:02:31 +0200

    fix(compat): role needs configuration
    * due to the change from role.name to role.id the roles doesn't work
      anymore on names. it could be that the database has mixed id types.
    
    * to overcome the possibility that the role has a non str id in the
      database it is possible to customize the needs according to the id in
      the database

--- invenio_records_lom/services/permissions/generators.py
+++ invenio_records_lom/services/permissions/generators.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2023-2024 Graz University of Technology.
+# Copyright (C) 2023-2026 Graz University of Technology.
 #
 # invenio-records-lom is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
 """Permission generators for permission policies."""
 
-from invenio_records_permissions.generators import Generator
+from typing import cast
 
-from .roles import oer_certified_user, oer_curator
+from flask import current_app
+from invenio_records_permissions.generators import Generator
 
 
 class OERCertifiedUsers(Generator):
@@ -17,7 +18,7 @@ class OERCertifiedUsers(Generator):
 
     def needs(self, **__: dict) -> list:
         """Generate needs to be checked against current user identity."""
-        return [oer_certified_user]
+        return cast(list, current_app.config.get("LOM_RECORD_CERTIFIED_USER_NEEDS", []))
 
 
 class OERCurators(Generator):
@@ -25,4 +26,4 @@ class OERCurators(Generator):
 
     def needs(self, **__: dict) -> list:
         """Generate needs to be checked against current user identity."""
-        return [oer_curator]
+        return cast(list, current_app.config.get("LOM_RECORD_CURATOR_USER_NEEDS", []))
